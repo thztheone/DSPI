dd0bc634aa236edf5ed1a532d010d311af1486d2
Matheus Santod 360x <137527894+MatheusSantos360@users.noreply.github.com>
2023-08-14 13:38:11 -0300

    Atualizar o Control.php
    a

--- model/Control/Control.php
+++ model/Control/Control.php
@@ -2,6 +2,25 @@
 
 class Control {
     
+    public $loginIndex = false;
+     
+    public function index() {
+        
+        if (isset($_SERVER["REQUEST_METHOD"])) {
+           
+            if ($_POST["email"]) {
+                $email = $_POST["email"];
+                $senha = $_POST["senha"];
+                $usuario = new Usuario();
+                
+                $this->$loginIndex = $usuario->validarUsuario($email, $senha);
+                
+                if ($loginIndex == 1) {
+                    header('location:home.php');
+                    exit;
+                } else {
+                    $this->loginIndex = "Dados inválidos";
+            }
+        }
+    }  
 }
-
-?>
